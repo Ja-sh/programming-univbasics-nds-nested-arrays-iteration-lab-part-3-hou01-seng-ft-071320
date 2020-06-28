e4c78d37eadc87f462c3e274248f5e3ed9faec42
Joshua Moore <morejoshua99@hotmail.com>
2020-06-28 02:49:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,10 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].is_a?(String)
+      #if src[row_index][element_index].respond_to?(:to_s)
       sen_array << src[row_index][element_index]
       elsif src[row_index][element_index].is_a?(Integer)
+      #elsif src[row_index][element_index].respond_to?(:to_i)
       int_array << src[row_index][element_index]
     end
     element_index += 1 
